0a4609a517276778609c8748a5dfe1d0084ffe41
dodomuk <siderid@Naver.com>
2021-06-01 18:04:38 +0900

    [210601동묵] 약수 & 배수

---
+++ A_StepByStep/src/q_hydrostatics_combinatorics/MultiplesAndMineralNumbers.java
@@ -0,0 +1,41 @@
+package q_hydrostatics_combinatorics;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class MultiplesAndMineralNumbers {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+
+		while (true) {
+
+			String input = br.readLine();
+			
+			if (input.equals("0 0")) {
+				break;
+			}
+			
+			st = new StringTokenizer(input, " ");
+			int num1 = Integer.parseInt(st.nextToken());
+			int num2 = Integer.parseInt(st.nextToken());
+
+			if (num2 % num1 == 0) {
+				sb.append("factor");
+			} else if (num1 % num2 == 0) {
+				sb.append("multiple");
+			} else {
+				sb.append("neither");
+			}
+
+			sb.append(System.lineSeparator());				
+			
+		}
+		System.out.println(sb);
+	}
+}
