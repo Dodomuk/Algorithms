3e188a136c8ebd561d2a48ca5349ad1d6c565b8e
dodomuk <siderid@Naver.com>
2021-06-01 18:08:39 +0900

    [210601동묵]배수 약수 리팩토링

--- A_StepByStep/src/q_hydrostatics_combinatorics/MultiplesAndMineralNumbers.java
+++ A_StepByStep/src/q_hydrostatics_combinatorics/MultiplesAndMineralNumbers.java
@@ -17,23 +17,19 @@ public class MultiplesAndMineralNumbers {
 
 			String input = br.readLine();
 			
-			if (input.equals("0 0")) {
-				break;
-			}
+			if (input.equals("0 0")) break;
 			
 			st = new StringTokenizer(input, " ");
 			int num1 = Integer.parseInt(st.nextToken());
 			int num2 = Integer.parseInt(st.nextToken());
 
 			if (num2 % num1 == 0) {
-				sb.append("factor");
+				sb.append("factor\n");
 			} else if (num1 % num2 == 0) {
-				sb.append("multiple");
+				sb.append("multiple\n");
 			} else {
-				sb.append("neither");
+				sb.append("neither\n");
 			}
-
-			sb.append(System.lineSeparator());				
 			
 		}
 		System.out.println(sb);
