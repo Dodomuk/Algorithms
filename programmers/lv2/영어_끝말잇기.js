97e4e73775ce162dcee9191eaf195416701e159e
unknown <siderid94@gmail.com>
2023-06-27 13:11:53 +0900

    06 27 코테 연습

---
+++ programmers/lv2/영어_끝말잇기.js
@@ -0,0 +1,31 @@
+function solution(n, words) {
+    const stack = [words[0]]
+    for (let index = 1; index < words.length; index++) {
+        // console.log('index', index)
+        // console.log('word 1', words[index])
+        // console.log('word 2', words[index - 1])
+        // console.log('stack', stack)
+        // console.log('include?', stack.includes(words[index]))
+        if (words[index - 1].charAt(words[index - 1].length - 1) !== words[index].charAt(0) || stack.includes(words[index])) {
+            return [((index + 1) % n) || n, ~~(index / n) + 1]
+        } else {
+            stack.push(words[index])
+        }
+    }
+    return [0, 0]
+}
+
+// 좋아요 👍🏼
+// function solution(n, words) {
+//     let answer = 0;
+//     words.reduce((prev, now, idx) => {
+//         answer = answer || ((words.slice(0, idx).indexOf(now) !== -1 || prev !== now[0]) ? idx : answer);
+//         return now[now.length - 1];
+//     }, "")
+
+//     return answer ? [answer % n + 1, Math.floor(answer / n) + 1] : [0, 0];
+// }
+
+console.log(solution(3, ["tank", "kick", "know", "wheel", "land", "dream", "mother", "robot", "tank"]))
+console.log(solution(5, ["hello", "observe", "effect", "take", "either", "recognize", "encourage", "ensure", "establish", "hang", "gather", "refer", "reference", "estimate", "executive"]))
+console.log(solution(2, ["hello", "one", "even", "never", "now", "world", "draw"]))
