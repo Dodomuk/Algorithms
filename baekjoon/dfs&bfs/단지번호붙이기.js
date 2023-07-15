3bb6a7939cb754d2fc14a589f8c8aaf47c35dfab
정동묵 <siderid94@gmail.com>
2023-07-15 11:12:22 +0900

    07 15 코테 연습

--- baekjoon/dfs&bfs/단지번호붙이기.js
+++ baekjoon/dfs&bfs/단지번호붙이기.js
@@ -1,44 +1,63 @@
-let arr = [
-  '0110100',
-  '0110101',
-  '1110101',
-  '0000111',
-  '0100000',
-  '0111110',
-  '0111000',
-];
-arr = arr.map((e) => e.split('').map((e) => parseInt(e)));
+// const fs = require('fs');
+// const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 
-function solution(n) {
-  let cnt = 0;
-  function dfs(x, y) {
-    console.log('x', x);
-    console.log('y', y);
+const input = `7
+0110100
+0110101
+1110101
+0000111
+0100000
+0111110
+0111001`
+  .toString()
+  .trim()
+  .split('\n');
 
-    if (arr[x][y]) {
-      cnt += 1;
-      arr[x][y] = 0;
-      if (arr[x][y + 1]) {
-        dfs(x, y + 1);
-      } else if (arr[x + 1][y]) {
-        dfs(x + 1, y);
-      } else {
-        console.log(cnt);
-        solution(n);
-      }
-    } else {
-        if (x < n) {
-          dfs(x + 1, y);
-        } else if (y < n) {
-          dfs(x, y + 1);
-        } else {
-          return;
-      } else {
-        dfs(x + 1, y);
+const n = parseInt(input.shift());
+const arr = input.map((e) => e.split('').map((e) => parseInt(e)));
+
+const returnArr = [];
+const xCheck = [-1, 1, 0, 0];
+const yCheck = [0, 0, -1, 1];
+const visitCheck = {};
+
+solution();
+
+function solution() {
+  for (let index_1 = 0; index_1 < n; index_1++) {
+    for (let index_2 = 0; index_2 < n; index_2++) {
+      if (arr[index_1][index_2] && !visitCheck[[index_1, index_2]]) {
+        bfs(index_1, index_2);
       }
     }
   }
-  dfs(0, 0);
+  console.log(returnArr.length);
+  returnArr.sort((a, b) => a - b).map((e) => console.log(e));
 }
 
-solution(7);
+function bfs(x, y) {
+  let cnt = 1;
+  visitCheck[[x, y]] = true;
+  const queue = [[x, y]];
+  while (queue.length) {
+    [x, y] = queue.shift();
+    for (let i = 0; i < 4; i++) {
+      const dx = xCheck[i] + x;
+      const dy = yCheck[i] + y;
+
+      if (
+        dx >= 0 &&
+        dy >= 0 &&
+        dx < n &&
+        dy < n &&
+        arr[dx][dy] &&
+        !visitCheck[[dx, dy]]
+      ) {
+        queue.push([dx, dy]);
+        visitCheck[[dx, dy]] = true;
+        cnt++;
+      }
+    }
+  }
+  if (cnt) returnArr.push(cnt);
+}
